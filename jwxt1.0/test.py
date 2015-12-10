3136f6e929b8d8f0fe2d135a564239bd13503131
luckyScript <jsjhlk@163.com>
2015-12-10 13:32:10 +0800

    repair

--- jwxt1.0/test.py
+++
@@ -1,4 +0,0 @@
-import urllib
-url = './my.xidian.edu.cn/ph/13060110001.jpg'  
-print "downloading with urllib"
-urllib.urlretrieve(url, "./test.jpg")
--- ryf_blog/ryf_img/test.py
+++
@@ -1,6 +0,0 @@
-import urllib
-for num in range(1,50):
-	str_num='%d'%num
-	url = 'http://www.ruanyifeng.com/images_pub/pub_'+str_num+'.jpg' 
-	print "downloading with urllib"
-	urllib.urlretrieve(url, "./"+str_num+".jpg")
