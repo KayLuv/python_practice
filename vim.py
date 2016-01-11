59fd0d19bfbac238940c69d89d6464c31efb320b
luckyScript <jsjhlk@163.com>
2016-01-11 12:47:42 +0800

    config vim

---
+++ vim.py
@@ -0,0 +1,21 @@
+#coding = utf-8
+def vimconfig():
+	'''
+	create or replace .vimrc file in ~
+	'''
+	#config contents
+	con = '''syntax on
+set number
+filetype on
+set nocompatible
+set history=1000
+set autoindent
+set smartindent
+set tabstop=4
+set shiftwidth=4
+set showmatch
+set incsearch'''
+	with open("~/.vimrc","w") as code:
+		code.write(con);
+vimconfig();
+
