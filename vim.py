1c0bbbb558bdd6704f6100960e25a8dbfb5ea18c
Huspy <mierhuo@gmail.com>
2018-01-31 20:23:40 +0800

    code format modification, remove semicolons

--- vim.py
+++ vim.py
@@ -1,21 +1,26 @@
-#coding = utf-8
-def vimconfig():
-	'''
-	create or replace .vimrc file in ~
-	'''
-	#config contents
-	con = '''syntax on
-set number
-filetype on
-set nocompatible
-set history=1000
-set autoindent
-set smartindent
-set tabstop=4
-set shiftwidth=4
-set showmatch
-set incsearch'''
-	with open("~/.vimrc","w") as code:
-		code.write(con);
-vimconfig();
+# coding: utf-8
+"""
+write vim configure file.
+"""
 
+def vimconfig():
+    '''
+    create or replace .vimrc file in ~
+    '''
+	#config contents
+    con = '''syntax on
+    set number
+	filetype on
+	set nocompatible
+	set history=1000
+	set autoindent
+	set smartindent
+	set tabstop=4
+	set shiftwidth=4
+	set showmatch
+	set incsearch'''
+
+    with open("~/.vimrc", "w") as code:
+        code.write(con)
+
+vimconfig()
